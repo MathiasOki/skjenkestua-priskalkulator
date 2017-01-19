c29b3c43c00acd86804d5a7944e1fea64663066e
Mathias Tollerud <mathias.tollerud@gmail.com>
2017-01-19 19:32:49 +0100

    Initial Commit

---
+++ Skjenkestua priskalkulatorTests/Skjenkestua_priskalkulatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Skjenkestua_priskalkulatorTests.swift
+//  Skjenkestua priskalkulatorTests
+//
+//  Created by Mathias Tollerud on 19.01.2017.
+//  Copyright © 2017 Mathias Tollerud. All rights reserved.
+//
+
+import XCTest
+@testable import Skjenkestua_priskalkulator
+
+class Skjenkestua_priskalkulatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
